a5e96bdee9bf39f74d06e6528ec09b2d0c185c62
zonaegomez <egomez@zonaluz.com.mx>
2026-05-05 16:17:11 -0600

    Add files via upload

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,3 @@
-
 import { createContext, useContext, useEffect, useState } from 'react'
 import { onAuthStateChanged } from 'firebase/auth'
 import { doc, getDoc, getDocs, collection, query, where } from 'firebase/firestore'
