4780d03551f583b402f1489707e17e6fa5acee2a
zonaegomez <egomez@zonaluz.com.mx>
2026-05-06 22:11:01 -0600

    Wrap App component with MonedaProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { MonedaProvider } from './context/MonedaContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MonedaProvider>
+      <App />
+    </MonedaProvider>
   </React.StrictMode>
 )
